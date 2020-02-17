8b51eb02726d0c32428f284537cbc4baf6ee98c3
Dmitry Patsura <talk@dmtry.me>
2020-02-17 17:17:59 +0300

    Feature: Introduce AsyncWorkerPool on top of Semaphore

---
+++ src/pool.ts
@@ -0,0 +1,21 @@
+import {Semaphore} from "./semaphore";
+
+export type AsyncWorker<P, R> = (task: P) => Promise<R>;
+
+export class AsyncWorkerPool<P, R = any> {
+    protected readonly semaphore: Semaphore;
+
+    constructor(
+        protected readonly worker: AsyncWorker<P, R>,
+        num: number
+    ) {
+        this.semaphore = new Semaphore(num);
+    }
+
+    public async execute(payload: P): Promise<R>
+    {
+        return await this.semaphore.execute(() => {
+            return this.worker(payload);
+        });
+    }
+}
