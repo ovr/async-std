e9211c618bfb4de1b29e3f1a51f5d5dce094860b
Dmitry Patsura <talk@dmtry.me>
2020-02-19 23:56:06 +0300

    Feature: Introduce AsyncStack

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './semaphore';
 export * from './mutex';
 export * from './pool';
 export * from './queue';
+export * from './stack';
 export * from './timers';
 export * from './ratelimit-pool';
